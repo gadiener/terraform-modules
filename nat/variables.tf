373b5c688527b754867c56b38de202d72fee0f49
Gabriele Diener <gabriele.diener@jobtome.com>
2020-01-28 18:43:35 +0100

    added cloud nat options

--- nat/variables.tf
+++ nat/variables.tf
@@ -14,7 +14,27 @@ variable "subnetwork" {
   description = "Router subnetwork link"
 }
 
+variable "min_ports_per_vm" {
+  description = "Minimum number of ports allocated to a VM"
+  default = "64"
+}
+
+variable "tcp_established_idle_timeout_sec" {
+  description = "Timeout in seconds for TCP established connections"
+  default = 1200
+}
+
+variable "tcp_transitory_idle_timeout_sec" {
+  description = "Timeout in seconds for TCP transitory connections"
+  default = 30
+}
+
 variable "address_count" {
   description = "Address count"
   default     = 1
+}
+
+variable "enable_error_log" {
+  description = "Enable error log"
+  default     = true
 }
